7a524a06b15a022d4f7d1a59c7773453c7f95be2
anggi-chandra <anggitjan4@gmail.com>
2025-05-24 00:18:49 +0800

    ngubah user dan juga product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// app/Models/Product.php
+
 class Product extends Model
 {
-    use HasFactory;
+    // Produk dimiliki oleh satu user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
